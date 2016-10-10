3ff61f07496ca7058aae285dc0c020ac2640cf7b
vianhanif <alvian524@gmail.com>
2016-10-10 12:57:51 +0700

    fix design warning urine

--- js/components/warning.js
+++ js/components/warning.js
@@ -6,12 +6,16 @@ const Warning = React.createClass({
           if(data.app.questions[1].warning){
             return(
               <div className="row">
-                <div className="col-xs-12">
+                <div className="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1">
                   <div className="panel-warning">
-                    <ul className="list-inline">
-                      <li><i className="fa fa-exclamation icon-warning"></i></li>
-                      <li className="text-warning"><span>{data.app.questions[1].warning}</span></li>
-                    </ul>
+                    <div className="row">
+                      <div className="col-xs-1 text-center">
+                        <i className="fa fa-exclamation icon-warning"></i>
+                      </div>
+                      <div className="col-xs-11 text-warning">
+                        <span>{data.app.questions[1].warning}</span>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
